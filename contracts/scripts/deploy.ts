0afffb7e1dcfe2aa80141ad062c1956c54535e9e
Ben DiFrancesco <ben@scopelift.co>
2021-07-06 18:02:17 -0400

    Setup test harness for Grant contracts and remove Greeter

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,22 +1,19 @@
-import { Contract, ContractFactory } from 'ethers';
+// import { Contract, ContractFactory } from 'ethers';
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from 'hardhat';
+// import { ethers } from 'hardhat';
 
 async function main(): Promise<void> {
   // Hardhat always runs the compile task when running scripts through it.
   // If this runs in a standalone fashion you may want to call compile manually
   // to make sure everything is compiled
   // await run("compile");
-
   // We get the contract to deploy
-  const Greeter: ContractFactory = await ethers.getContractFactory('Greeter');
-  const greeter: Contract = await Greeter.deploy('Hello, Buidler!');
-  await greeter.deployed();
-
-  console.log('Greeter deployed to: ', greeter.address);
+  // const Greeter: ContractFactory = await ethers.getContractFactory('Greeter');
+  // const greeter: Contract = await Greeter.deploy('Hello, Buidler!');
+  // await greeter.deployed();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
