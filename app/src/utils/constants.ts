6f243dd5469dee9d6cc044224a7c1a3376bf6c5b
Matt Solomon <matt@mattsolomon.dev>
2021-07-13 14:37:46 -0700

    read and display data from the GrantRegistry contract

--- app/src/utils/constants.ts
+++ app/src/utils/constants.ts
@@ -3,7 +3,6 @@ export const RPC_URL = `https://mainnet.infura.io/v3/${import.meta.env.VITE_INFU
 
 // Read data using Multicall2: https://github.com/makerdao/multicall
 export const MULTICALL_ADDRESS = '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696'; // applies to mainnet, rinkeby, goerli, ropsten, kovan
-
 export const MULTICALL_ABI = [
   'function getCurrentBlockTimestamp() view returns (uint256 timestamp)',
   'function aggregate(tuple(address target, bytes callData)[] calls) view returns (uint256 blockNumber, bytes[] returnData)',
@@ -16,3 +15,7 @@ export const MULTICALL_ABI = [
   'function tryAggregate(bool requireSuccess, tuple(address target, bytes callData)[] calls) public view returns (tuple(bool success, bytes returnData)[] returnData)',
   'function tryBlockAndAggregate(bool requireSuccess, tuple(address target, bytes callData)[] calls) public view returns (uint256 blockNumber, bytes32 blockHash, tuple(bool success, bytes returnData)[] returnData)',
 ];
+
+// Data for Grants contracts
+export const GRANT_REGISTRY_ADDRESS = '0x70e0bA845a1A0F2DA3359C97E0285013525FFC49';
+export { abi as GRANT_REGISTRY_ABI } from '@dgrants/contracts/artifacts/contracts/GrantRegistry.sol/GrantRegistry.json';
--- app/src/views/GrantRegistryList.vue
+++ app/src/views/GrantRegistryList.vue
@@ -1,11 +1,51 @@
 <template>
-  <div>Grant Registry List</div>
+  <h1 class="text-lg mb-3">Grant Registry List</h1>
+  <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+    <li v-for="grant in grants" :key="grant.id" class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
+      <div class="w-full flex items-center justify-between p-6 space-x-6">
+        <div class="flex-1 truncate text-left">
+          <div class="flex items-center space-x-3">
+            <h3 class="text-gray-900 text-sm font-medium truncate">Grant ID: {{ grant.id.toString() }}</h3>
+          </div>
+          <p class="mt-1 text-gray-500 text-sm truncate">{{ grant.metaPtr }}</p>
+        </div>
+      </div>
+      <div>
+        <div class="pl-6 p-2 -mt-px flex divide-x divide-gray-200">
+          <div class="w-0 flex-1 flex">
+            <div class="flex-1 truncate text-left">
+              <p class="mt-1 text-gray-500 text-sm truncate">Owner</p>
+              <div class="flex items-center space-x-3">
+                <h3 class="text-gray-900 text-sm font-medium">{{ formatAddress(grant.owner) }}</h3>
+              </div>
+            </div>
+          </div>
+          <div class="pl-6 -ml-px w-0 flex-1 flex">
+            <div class="w-0 flex-1 flex">
+              <div class="flex-1 truncate text-left">
+                <p class="mt-1 text-gray-500 text-sm truncate">Payee</p>
+                <div class="flex items-center space-x-3">
+                  <h3 class="text-gray-900 text-sm font-medium">{{ formatAddress(grant.payee) }}</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { formatAddress } from 'src/utils/formatters';
+import useDataStore from 'src/store/data';
 
 export default defineComponent({
   name: 'GrantRegistryList',
+  setup() {
+    const { grants } = useDataStore();
+    return { formatAddress, grants };
+  },
 });
 </script>
