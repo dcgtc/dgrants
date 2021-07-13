f665bc3e738817d74ca5a31339519d360d1ebb3d
Matt Solomon <matt@mattsolomon.dev>
2021-07-12 19:44:00 -0700

    dgrants page and router setup

---
+++ app/src/views/GrantRegistryNewGrant.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Grant Registry New Grant</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'GrantRegistryNewGrant',
+});
+</script>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -6,6 +6,11 @@
     <div>Date: {{ date }}</div>
     <div>Balance: {{ balance }} ETH</div>
   </div>
+
+  <div class="mt-3">
+    <button @click="push('dgrants')" class="btn btn-primary mr-3">Grants Registry</button>
+    <button @click="push('dgrants-new')" class="btn btn-secondary">Create New Grant</button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -13,17 +18,24 @@ import { computed, defineComponent } from 'vue';
 import useDataStore from 'src/store/data';
 import useWalletStore from 'src/store/wallet';
 import { commify, formatUnits } from 'src/utils/ethers';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
   name: 'Home',
   setup() {
+    // Router navigation
+    const router = useRouter();
+    function push(name: string) {
+      router.push({ name });
+    }
+
     const { lastBlockNumber, lastBlockTimestamp, ethBalance } = useDataStore();
     const { userDisplayName, network } = useWalletStore();
 
     const blockNumber = computed(() => commify(lastBlockNumber.value));
     const date = computed(() => new Date(lastBlockTimestamp.value * 1000).toLocaleString());
     const balance = computed(() => (ethBalance.value ? Number(formatUnits(ethBalance.value)).toFixed(4) : 0));
-    return { userDisplayName, network, blockNumber, date, balance, formatUnits };
+    return { push, userDisplayName, network, blockNumber, date, balance, formatUnits };
   },
 });
 </script>
