05a2c91886f2cc1ee97c7b188a5d4842e9a009fb
Matt Solomon <matt@mattsolomon.dev>
2021-07-14 06:51:58 -0700

    setup grant details page

--- app/src/views/GrantRegistryGrantDetail.vue
+++ app/src/views/GrantRegistryGrantDetail.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>Grant Registry Grant Detail</div>
+  <div class="font-bold">Details for Grant ID {{ grant.id.toString() }}</div>
+  <p>Owner: {{ grant.owner }}</p>
+  <p>Payee: {{ grant.payee }}</p>
+  <p>
+    Metadata URL: <a class="link" :href="grant.metaPtr" target="_blank">{{ grant.metaPtr }}</a>
+  </p>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useRoute } from 'vue-router';
+import useDataStore from 'src/store/data';
 
 export default defineComponent({
   name: 'GrantRegistryGrantDetail',
+  setup() {
+    const route = useRoute();
+    const { grants } = useDataStore();
+    return { grant: grants.value[route.params.id] };
+  },
 });
 </script>
