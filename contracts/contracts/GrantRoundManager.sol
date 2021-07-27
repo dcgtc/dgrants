d7dad5036de881fbd1b1475fa36c30d2c845cbf8
Matt Solomon <matt@mattsolomon.dev>
2021-07-27 07:47:06 -0700

    Move donationToken to be defined at GrantRoundManager deployment

--- contracts/contracts/GrantRoundManager.sol
+++ contracts/contracts/GrantRoundManager.sol
@@ -1,18 +1,26 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.5;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./GrantRound.sol";
 
 contract GrantRoundManager {
+  /// @notice Address of the ERC20 token in which donations are made
+  IERC20 public immutable donationToken;
+
   /// @notice Emitted when a new GrantRound contract is created
   event GrantRoundCreated(address grantRound);
+
+  constructor(IERC20 _donationToken) {
+    require(_donationToken.totalSupply() > 0, "GrantRoundManager: Invalid token");
+    donationToken = _donationToken;
+  }
 
   /**
    * @notice Creates a new GrantRound
    * @param _owner Grant round owner that has permission to update the metadata pointer
    * @param _payoutAdmin Grant round administrator that has permission to payout the matching pool
    * @param _registry Address that contains the grant metadata
-   * @param _donationToken Address of the ERC20 token in which donations are made
    * @param _startTime Unix timestamp of the start of the round
    * @param _endTime Unix timestamp of the end of the round
    * @param _metaPtr URL pointing to the grant round metadata
@@ -22,7 +30,6 @@ contract GrantRoundManager {
     address _owner,
     address _payoutAdmin,
     GrantRegistry _registry,
-    IERC20 _donationToken,
     uint256 _startTime,
     uint256 _endTime,
     string memory _metaPtr,
@@ -32,7 +39,7 @@ contract GrantRoundManager {
       _owner,
       _payoutAdmin,
       _registry,
-      _donationToken,
+      donationToken,
       _startTime,
       _endTime,
       _metaPtr,
--- contracts/test/GrantRoundManager.test.ts
+++ contracts/test/GrantRoundManager.test.ts
@@ -3,6 +3,7 @@ import { artifacts, ethers, waffle } from 'hardhat';
 import { Artifact } from 'hardhat/types';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 import { deployMockContract } from '@ethereum-waffle/mock-contract';
+import { MockContract } from 'ethereum-waffle';
 import { expect } from 'chai';
 
 // --- Our imports ---
@@ -17,31 +18,45 @@ const randomAddress = () => ethers.Wallet.createRandom().address;
 describe('GrantRoundManager', () => {
   let user: SignerWithAddress;
   let manager: GrantRoundManager;
+  let mockToken: MockContract;
 
   before(async () => {
     [user] = await ethers.getSigners();
+    mockToken = await deployMockContract(user, ['function totalSupply() returns(uint256)']);
+    await mockToken.mock.totalSupply.returns('1');
 
     // Deploy Manager
     const managerArtifact: Artifact = await artifacts.readArtifact('GrantRoundManager');
-    manager = <GrantRoundManager>await deployContract(user, managerArtifact);
+    manager = <GrantRoundManager>await deployContract(user, managerArtifact, [mockToken.address]);
   });
 
   it('deploys properly', async () => {
     expect(isAddress(manager.address), 'Failed to deploy GrantRoundManager').to.be.true;
   });
 
+  it('reverts when deploying with an invalid donation token', async () => {
+    // First we test a donation token address that has no code
+    const managerArtifact: Artifact = await artifacts.readArtifact('GrantRoundManager');
+    await expect(deployContract(user, managerArtifact, [randomAddress()])).to.be.revertedWith(
+      'function call to a non-contract account'
+    );
+
+    // Now we test a donation token that has no supply
+    await mockToken.mock.totalSupply.returns('0');
+    await expect(deployContract(user, managerArtifact, [mockToken.address])).to.be.revertedWith(
+      'GrantRoundManager: Invalid token'
+    );
+  });
+
   it('creates new grant rounds', async () => {
     // Deploy and configure mocks (used to pass the validation in the GrantRound constructor)
     const mockRegistry = await deployMockContract(user, ['function grantCount() returns(uint96)']);
     await mockRegistry.mock.grantCount.returns('0');
-    const mockToken = await deployMockContract(user, ['function totalSupply() returns(uint256)']);
-    await mockToken.mock.totalSupply.returns('1');
 
     // Create round
     const metadataAdmin = randomAddress();
     const payoutAdmin = randomAddress();
     const registry = mockRegistry.address;
-    const token = mockToken.address;
     const startTime = '50000000000000'; // random timestamp far in the future
     const endTime = '60000000000000'; // random timestamp far in the future
     const metaPtr = 'https://metadata-pointer.com';
@@ -50,7 +65,6 @@ describe('GrantRoundManager', () => {
       metadataAdmin,
       payoutAdmin,
       registry,
-      token,
       startTime,
       endTime,
       metaPtr,
@@ -70,7 +84,7 @@ describe('GrantRoundManager', () => {
     expect(await grantRound.metadataAdmin()).to.equal(metadataAdmin);
     expect(await grantRound.payoutAdmin()).to.equal(payoutAdmin);
     expect(await grantRound.registry()).to.equal(registry);
-    expect(await grantRound.donationToken()).to.equal(token);
+    expect(await grantRound.donationToken()).to.equal(mockToken.address);
     expect(await grantRound.startTime()).to.equal(startTime);
     expect(await grantRound.endTime()).to.equal(endTime);
     expect(await grantRound.metaPtr()).to.equal(metaPtr);
