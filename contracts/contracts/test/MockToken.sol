90570286d1c071a6f6ec27015e6c23bbe3ead0dd
Cory Dickson <cory.dickson@protonmail.com>
2021-07-29 14:17:25 -0400

    Add external mint function

--- contracts/contracts/test/MockToken.sol
+++ contracts/contracts/test/MockToken.sol
@@ -3,7 +3,11 @@ pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockToken is ERC20 {
-  constructor(uint256 initialSupply) public ERC20("Mock", "MCK") {
+  constructor(uint256 initialSupply) ERC20("Mock", "MCK") {
+    _mint(msg.sender, initialSupply);
+  }
+
+  function mint(uint256 initialSupply) external {
     _mint(msg.sender, initialSupply);
   }
 }
