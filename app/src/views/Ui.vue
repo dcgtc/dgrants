68ac7c773dd0b00f711d9e21255e0c358ce37b88
Richard Schumann <schumanncombo@gmail.com>
2021-08-04 21:37:41 +0200

    test to find out whats the commit issue

--- app/src/views/Ui.vue
+++ app/src/views/Ui.vue
@@ -1,5 +1,4 @@
 <template>
-  
   <!-- this is a modular base header. 
   if there is a subline, it displays it.
   if there is an address, it displays time, action, address ( by scco.eth last update 3 days ago )
@@ -9,30 +8,40 @@
   tbd: what if no left right arrow : then justify-between not work nice... just keep it but hide with opacity? or better way to align this so it works also without that nav ?
   -->
 
-  <BaseHeader 
-      headline='Edit Collection' 
-      subline='“Best GR11 Gems”'
-      address='gitcoin-dao.eth'
-      action="updated"
-      time='3 minutes ago'
-      text='“Focus on infrastructure, and the organization of services. vidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.”'
+  <BaseHeader
+    headline="Edit Collection"
+    subline="“Best GR11 Gems”"
+    address="gitcoin-dao.eth"
+    action="updated"
+    time="3 minutes ago"
+    text="“Focus on infrastructure, and the organization of services. vidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.”"
   />
 
-
+  <p>test</p>
 
   <h1>I am a Headline</h1>
 
-  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+  <p>
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+    magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+    gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+    elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+    eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+    dolor sit amet.
+  </p>
 
-  <p class="intent">Im a paragraph but with intent. <a href="#">link</a> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-
+  <p class="intent">
+    Im a paragraph but with intent. <a href="#">link</a> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+    diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+    accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
+    sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+    et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+    clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+  </p>
 
   <Button label="Click Me" />
 
   <a href="#">Im a link</a>
-
-
-
 </template>
 
 <script lang="ts">
@@ -45,4 +54,3 @@ export default defineComponent({
   components: { Button, BaseHeader },
 });
 </script>
-
