17133de9d645a163a0ebc57efa019b2745e10db3
Cory Dickson <cory.dickson@protonmail.com>
2021-08-04 19:40:46 -0400

    Update param comment for matchingToken

--- contracts/contracts/GrantRoundManager.sol
+++ contracts/contracts/GrantRoundManager.sol
@@ -65,7 +65,7 @@ contract GrantRoundManager {
    * @notice Creates a new GrantRound
    * @param _owner Grant round owner that has permission to update the metadata pointer
    * @param _payoutAdmin Grant round administrator that has permission to payout the matching pool
-   * @param _matchingToken Address for the matching pool tokens
+   * @param _matchingToken Address for the token used to payout match amounts at the end of a round
    * @param _registry Address that contains the grant metadata
    * @param _startTime Unix timestamp of the start of the round
    * @param _endTime Unix timestamp of the end of the round
