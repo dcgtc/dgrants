6b3de88a315110f92a02f43f226b0fd6277f3c75
Richard Schumann <schumanncombo@gmail.com>
2021-08-05 13:47:28 +0200

    reworked header
    and removed button and header component to not care for vue / logic for now but just get the ui quicker in place .

--- app/src/utils/utils.ts
+++ app/src/utils/utils.ts
@@ -8,7 +8,7 @@ import { isAddress } from 'src/utils/ethers';
 // Returns an address with the following format: 0x1234...abcd
 export function formatAddress(address: string) {
   if (address.length !== 42) return null;
-  return `${address.slice(0, 6)}...${address.slice(38)}`;
+  return `${address.slice(0, 6)}…${address.slice(38)}`;
 }
 
 // Navigates to the specified page and pushes a new entry into the history stack
