3bdc8783461a1f63318a5efc8a2d7b1be120c54e
Matt Solomon <matt@mattsolomon.dev>
2021-08-12 14:58:05 -0700

    feat: setup cart page

--- app/src/types/index.ts
+++ app/src/types/index.ts
@@ -1,1 +1,6 @@
 // App-specific type definition go here
+export type CartItemOptions = {
+  grantId: string;
+  contributionTokenAddress: string; // store address instead of TokenInfo to reduce localStorage size used
+  contributionAmount: string;
+};
