1b84b793b94bc0523a4d3299ac82f729165f12aa
Richard Schumann <schumanncombo@gmail.com>
2021-08-13 17:41:53 +0200

    Update About.vue

--- app/src/components/About.vue
+++ app/src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     :class="[showAbout ? 'flex' : 'hidden']"
-    class="h-screen fixed"
+    class="h-screen fixed w-full"
     style="z-index: 10000; background-color: #ffffffdd"
   >
     <div class="px-4 md:px-12 m-auto bg-white">
@@ -33,8 +33,16 @@
           >, <span><a href="#">Name</a></span
           >, <span><a href="#">Name</a></span
           >, <span><a href="#">Name</a></span
+          >, <span><a href="#">Name</a></span
+          >, <span><a href="#">Name</a></span
+          >, <span><a href="#">Name</a></span
+          >, <span><a href="#">Name</a></span
+          >, <span><a href="#">Name</a></span
+          >, <span><a href="#">Name</a></span
+          >, <span><a href="#">Name</a></span
+          >, <span><a href="#">Name</a></span
+          >, <span><a href="#">Name</a></span
           >,
-          <span><a href="#">Name</a></span>
         </div>
 
         <!-- donate -->
