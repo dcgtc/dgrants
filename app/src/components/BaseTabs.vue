c1f1b49610f71d0bc6845cd58cb160713e43657f
Richard Schumann <schumanncombo@gmail.com>
2021-08-16 16:26:21 +0200

    simplify css for BaseTabs

--- app/src/components/BaseTabs.vue
+++ app/src/components/BaseTabs.vue
@@ -4,20 +4,11 @@ where one is the "active" by apply "border-b-2" class on it.
 -->
 
 <template>
-
-<div class="px-4 md:px-12 py-6 border-b border-grey-100 flex flex-wrap gap-x-10 gap-y-2">
-
-  <span class="cursor-pointer text-grey-500 font-medium no-underline
-  whitespace-pre-line hover:text-grey-500 py-2 border-b-2">All (543)</span>
-
-  <span class="cursor-pointer text-grey-400 font-medium no-underline
-  whitespace-pre-line hover:text-grey-500 py-2">Infra Tech (184)</span>
-
-  <span class="cursor-pointer text-grey-500 font-medium no-underline
-  whitespace-pre-line hover:text-grey-400 py-2">dApp Tech (184)</span>
-
-</div>
-
+  <div class="px-4 md:px-12 py-6 border-b border-grey-100 flex flex-wrap gap-x-10 gap-y-2">
+    <span class="tab active">All (543)</span>
+    <span class="tab">Infra Tech (184)</span>
+    <span class="tab">dApp Tech (184)</span>
+  </div>
 </template>
 
 <script lang="ts">
@@ -28,3 +19,13 @@ export default defineComponent({
   components: {},
 });
 </script>
+
+<style scoped>
+.tab {
+  @apply cursor-pointer text-grey-400 font-medium no-underline whitespace-pre-line hover:text-grey-500 py-2;
+}
+
+.tab.active {
+  @apply border-b-2;
+}
+</style>
