bc0b2c6ac9e27aa3fc0918770fcca35a89fbc9f8
Matt Solomon <matt@mattsolomon.dev>
2021-08-16 15:19:58 -0700

    chore: move GrantMetadata type definition and run 'yarn' from root

--- app/src/types/index.ts
+++ app/src/types/index.ts
@@ -1,5 +1,5 @@
 // App-specific type definition go here
-import { Grant } from '@dgrants/types';
+import { Grant, GrantMetadata } from '@dgrants/types';
 import { TokenInfo } from '@uniswap/token-lists';
 
 // Cart info saved in localStorage
@@ -7,12 +7,6 @@ export type CartItemOptions = {
   grantId: string;
   contributionTokenAddress: string; // store address instead of TokenInfo to reduce localStorage size used
   contributionAmount: number; // store as a human-readable number for better form UX
-};
-
-export type GrantMetadata = {
-  name: string;
-  description: string;
-  logoURI: string;
 };
 
 // Cart info used by the main Cart component
