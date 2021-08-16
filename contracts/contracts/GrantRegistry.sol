4995e25102ea10c784001902935243fad06afc23
mkdir <projectdevelopinginbox@gmail.com>
2021-08-16 11:07:33 -0500

    with code commented

--- contracts/contracts/GrantRegistry.sol
+++ contracts/contracts/GrantRegistry.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.5;
+pragma solidity ^0.7.6;
+pragma abicoder v2;
 
 /**
  * @notice The Gitcoin GrantRegistry contract keeps track of all grants that have been created.
