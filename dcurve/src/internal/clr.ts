8c7f9393e4c49a55bf1b400a70dd5f8dbd2eed93
Graham Dixon <graham@gdixon.co.uk>
2021-08-19 15:19:04 +0100

    Finishes and refactors clr and merkle

---
+++ dcurve/src/internal/clr.ts
@@ -0,0 +1,146 @@
+'use strict';
+
+import {
+  CLRArgs,
+  GrantMatch,
+  GrantPrediction,
+  GrantPredictionArgs,
+  GrantPredictions,
+  GrantRoundContributions,
+  GrantsDistribution,
+  InitArgs,
+  PayoutMatch,
+  PayoutMatches,
+} from '../../src/types';
+import { generateMerkle, getMerkleRoot } from './merkle';
+import { addAnonContribution, getGrantMatch } from './utils';
+
+// polyfill Buffer for use in browser
+import { Buffer } from 'buffer/';
+// set to window (TODO: make this work in vite.config.ts)
+Object.defineProperty(window, 'Buffer', { value: Buffer });
+
+export class CLR {
+  _options: InitArgs;
+
+  constructor(options: InitArgs) {
+    this._options = options;
+  }
+
+  calculate(grantRoundContributions: GrantRoundContributions, _options?: Record<string, unknown>): GrantsDistribution {
+    // allow the options to be overridden
+    const options = Object.assign(this._options, _options || {});
+
+    // retrieve the calcAlgo from the supplied options
+    const calcAlgo = options['calcAlgo'];
+
+    // move distribution into a payout obj
+    const payoutObj: PayoutMatches = {};
+
+    // calculate distribution based on contributions
+    const distribution: GrantsDistribution = calcAlgo({
+      contributions: grantRoundContributions,
+      ...options,
+    } as CLRArgs);
+
+    // record the grantRound for identification
+    distribution.grantRound = grantRoundContributions.grantRound;
+
+    // check if the calculation should include payouts (we cannot create a merkle without any contributions)
+    if (options.includePayouts && distribution.distribution.length) {
+      // construct the payout obj by combining any grants which have the same payout address
+      distribution.distribution.forEach((grantMatch: GrantMatch) => {
+        // each payout address must only appear once in the payoutDistribution
+        let grantIds = [grantMatch.grantId];
+        // check if the payout address is already recorded
+        if (payoutObj[grantMatch.address]) {
+          // get the current set of grantIds
+          const currentGrantIds = payoutObj[grantMatch.address].grantIds;
+          // add the set of grantIds to the arr and filter duplicates
+          grantIds = grantIds.concat(currentGrantIds).filter((item, pos) => currentGrantIds.indexOf(item) === pos);
+        }
+        // ensure the payout address has a match to claim
+        if (grantMatch.match) {
+          // sum the total match for each payout address
+          payoutObj[grantMatch.address] = {
+            grantIds: grantIds,
+            address: grantMatch.address,
+            match: grantMatch.match + (payoutObj[grantMatch.address]?.match || 0),
+          } as PayoutMatch;
+        }
+      });
+      // record the distribution used to generate the merkle
+      distribution.payoutDistribution = Object.values(payoutObj) as PayoutMatch[];
+      // generate the merkle for the payouts
+      distribution.merkle = generateMerkle(distribution.payoutDistribution, grantRoundContributions.currDecimals);
+      // set the merkleRoot as hash to allow payouts to be verifiable
+      distribution.hash = getMerkleRoot(distribution.merkle);
+    }
+
+    // return distribution details (<GrantsDistribution>)
+    return distribution;
+  }
+
+  /**
+   * Calculates the matching amount based on current distribution and predicts
+   * the match based on predictionPoints
+   * @param GrantPredictionArgs
+   *
+   * @returns GrantPredictions
+   */
+  predict(args: GrantPredictionArgs, _options?: Record<string, unknown>): GrantPredictions {
+    // allow the options to be overridden
+    const options = Object.assign(this._options, _options || {});
+
+    // retrieve the calcAlgo from the supplied options
+    const calcAlgo = options['calcAlgo'];
+
+    // unpack the prediction arguments
+    const grantId: number = args.grantId;
+    const predictionPoints: number[] = args.predictionPoints;
+    const grantRoundContributions: GrantRoundContributions = args.grantRoundContributions;
+    const predictions: GrantPrediction[] = [];
+
+    // calculate distribution based on current contribution
+    const distribution: GrantsDistribution = calcAlgo({
+      contributions: grantRoundContributions,
+      ...(options || {}),
+    } as CLRArgs);
+
+    // retrieve only this grantId from the distribution
+    const currentGrantMatch = getGrantMatch(grantId, distribution);
+
+    // calculate predicted distribution for each predictionPoint
+    predictionPoints.forEach((predictionPoint) => {
+      // calculate distribution with anon contribution
+      const newDistribution: GrantsDistribution = calcAlgo({
+        // add anon contribution of value predictionPoint
+        contributions: addAnonContribution(grantId, { ...grantRoundContributions }, predictionPoint),
+        // allow for overrides to set calc algo
+        ...options,
+      } as CLRArgs);
+
+      // retrieve only this grantId from the predicted distribution
+      const predictedGrantMatch = getGrantMatch(grantId, newDistribution);
+
+      // push the prediction for the predictionPoint
+      predictions.push({
+        predictionPoint: predictionPoint,
+        predictedGrantMatch: predictedGrantMatch,
+        predictionDiff: predictedGrantMatch - currentGrantMatch,
+      } as GrantPrediction);
+    });
+
+    // return the GrantPredictions
+    return {
+      grantId: grantId,
+      grantRound: grantRoundContributions.grantRound,
+      predictions: predictions,
+    } as GrantPredictions;
+  }
+}
+
+// ======= FOR FUTURE ========
+
+// how to store the distribution curve as opposed to
+// ability to pass in multiple GrantRound and generate total distribution
---
+++ dcurve/src/internal/merkle.ts
@@ -0,0 +1,98 @@
+import { parseUnits } from 'ethers/lib/utils';
+import {
+  MerkleDistributorInfo,
+  NewFormat,
+  parseBalanceMap,
+} from '@dgrants/utils/src/merkle-distributor/parse-balance-map';
+import { PayoutMatch } from '../types';
+
+// export MerkleDistributorInfo
+export type { MerkleDistributorInfo };
+
+/**
+ * Generates hash of the GrantsDistribution distribution
+ * @param distribution - PayoutMatch[]
+ * @param currDecimals - The number of decimals used by the distributed token
+ * @returns {MerkleDistributorInfo} - merkle tree of the distribution
+ * https://github.com/Uniswap/merkle-distributor
+ */
+export const generateMerkle = (distribution: PayoutMatch[], currDecimals: number) => {
+  // collect the input
+  const merkleInput: NewFormat[] = [];
+
+  // foreach payout parse the earnings
+  distribution.forEach((payoutMatch: PayoutMatch) => {
+    merkleInput.push({
+      address: payoutMatch.address,
+      earnings: parseUnits(payoutMatch.match.toString(), currDecimals).toHexString(),
+      reasons: '',
+    });
+  });
+
+  // parse and return the tree
+  return parseBalanceMap(merkleInput) as MerkleDistributorInfo;
+};
+
+/**
+ * Generates hash of the GrantsDistribution distribution
+ * @param distribution - PayoutMatch[]
+ * @param currDecimals - The number of decimals used by the distributed token
+ * @returns {string} - merkle root hash of the distribution
+ * https://github.com/Uniswap/merkle-distributor
+ */
+export const generateMerkleRoot = (distribution: PayoutMatch[], currDecimals: number): string => {
+  // get the merkleTree
+  const merkleDistributorInfo: MerkleDistributorInfo = generateMerkle(distribution, currDecimals);
+
+  // return merkleRoot
+  return merkleDistributorInfo.merkleRoot;
+};
+
+/**
+ * Generates merkle and outputs the proof of the GrantsDistribution distribution
+ * @param address - address to find in the distribution
+ * @param distribution - PayoutMatch[]
+ * @param currDecimals - The number of decimals used by the distributed token
+ * @returns {string[]} - merkle proof for the provided address
+ * https://github.com/Uniswap/merkle-distributor
+ */
+export const generateMerkleProof = (address: string, distribution: PayoutMatch[], currDecimals: number): string[] => {
+  // get the merkleTree
+  const merkleDistributorInfo: MerkleDistributorInfo = generateMerkle(distribution, currDecimals);
+
+  // return proof for address
+  return merkleDistributorInfo.claims[address].proof;
+};
+
+/**
+ * Gets a proof from the provided merkle tree
+ * @param merkleDistributorInfo - MerkleDistributorInfo
+ * @returns {string} - merkle root hash of the distribution
+ * https://github.com/Uniswap/merkle-distributor
+ */
+export const getMerkleRoot = (merkleDistributorInfo: MerkleDistributorInfo | undefined): string => {
+  // when merkleDistributorInfo is provided
+  if (merkleDistributorInfo) {
+    // return root of the merkle
+    return merkleDistributorInfo.merkleRoot;
+  } else {
+    return '';
+  }
+};
+
+/**
+ * Gets a proof from the provided merkle tree
+ * @param address - address to find in the claims
+ * @param merkleDistributorInfo - MerkleDistributorInfo
+ * @returns {string[]} - merkle proof for the provided address
+ * https://github.com/Uniswap/merkle-distributor
+ */
+export const getMerkleProof = (address: string, merkleDistributorInfo: MerkleDistributorInfo | undefined): string[] => {
+  // when merkleDistributorInfo is provided
+  if (merkleDistributorInfo) {
+    // return proof for address
+    return merkleDistributorInfo.claims[address].proof;
+  } else {
+    return [];
+  }
+};
