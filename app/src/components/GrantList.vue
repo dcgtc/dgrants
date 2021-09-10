f4e39c6b451ceae6ec58982140200fd6104a579d
carletex <oceanrdn@gmail.com>
2021-09-10 16:06:05 +0200

    Show placeholder if grant image is not available

--- app/src/components/GrantList.vue
+++ app/src/components/GrantList.vue
@@ -4,13 +4,12 @@
     <ul class="text-left grid grid-cols-1 gap-10 py-10 sm:grid-cols-2">
       <li v-for="grant in grants" :key="grant.id.toString()">
         <!-- ToDo: Raised data -->
-        <!-- ToDo: Grant image -->
         <GrantCard
           :id="grant.id"
           :name="grantMetadata[grant.metaPtr].name ?? ''"
           :ownerAddress="grant.owner"
+          :imgurl="grantMetadata[grant.metaPtr].logoURI ?? '/src/assets/placeholder_grant.svg'"
           raised="100"
-          imgurl="https://s3-alpha-sig.figma.com/img/901c/c3ae/ac690f9a43a7634d9d198cf9057c4454?Expires=1632096000&Signature=PnDBcqyesPYCxYzyRTGn-imh2ktN8ImD4GIrtJKoBrpooTX6Uqae5ogcpWdMpn6CyZnfDAnGuA2XYokhIpLTbLTriqd5Ifiiv2zbVHHQlUwQQ8fk4k9gpAj17CTBJGS0Ldqv6pn9MvSTzXKaLa~FjSiX2AAeWgWz1Vy9OJ6KFahsmhfLXlGEmBDr1Dx016AMvWH5DhKRLY4XbpfJz5zJy6zEHbV7GZ6uxop4d-pIk9KvTPt3kjJEsmKRdVxxy8XC8zN2cIFMEQEWJDRe64AekEl5ABzlTN~9o8NYS6~q5ObH3BlwQuvUhPLNvw0Atjwc9DX3yWuo-cAOZISl5sboOg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
         />
       </li>
     </ul>
