041a36f436aec94098f21314d088b8c7002a88c8
Richard Schumann <schumanncombo@gmail.com>
2021-09-13 18:39:18 +0200

    improve InutRow
    - get rid of the left empty area for X icon
    - get rid of "toggleable button" as we do not use it + was not working propper anyway
    - get rid of complex css with better understandable code
    - have a good solution for X icon when the row is "deletable"
    
    Closes The Issue that i can not find from todays standup where ben showed mobile input fields ...
    
    this does not affect any changes in any forms, just merge and done.

--- app/src/components/InputRow.vue
+++ app/src/components/InputRow.vue
@@ -1,66 +1,23 @@
 <template>
   <div :class="intended ? 'px-4 md:px-12' : ''">
     <div class="py-8 border-b border-grey-100" :class="intended ? '' : 'px-4 md:px-12'">
-      <div class="grid grid-cols-12 items-center gap-x-8">
+      <div class="grid grid-cols-4 items-center gap-x-8 gap-y-4">
         <!-- label -->
-        <div class="col-span-12 md:col-span-3 mb-3 md:mb-0">
+        <div class="col-span-4 md:col-span-1">
           <slot name="label"></slot>
         </div>
 
         <!-- input + optional description -->
-        <div class="col-span-9 md:col-span-6">
-          <slot name="input"></slot>
+        <div class="col-span-4 md:col-span-3">
+          <div class="flex items-center gap-x-8">
+            <slot name="input"></slot>
+
+            <!-- deletable -->
+            <div v-if="deleteable" class="col-span-3 justify-self-end">
+              <XIcon class="icon icon-primary icon-small cursor-pointer" />
+            </div>
+          </div>
           <div v-if="text" class="mt-2">{{ text }}</div>
-        </div>
-
-        <!-- deletable -->
-        <div v-if="deleteable" class="col-span-3 justify-self-end">
-          <XIcon class="icon icon-primary icon-small cursor-pointer" />
-        </div>
-
-        <!--toggle-->
-        <div v-if="toggleable" class="col-span-3 justify-self-end">
-          <input v-model="toggle" type="checkbox" class="hidden" checked />
-          <div
-            v-if="toggle"
-            @click="
-              () => {
-                toggle = !toggle;
-              }
-            "
-            class="
-              px-8
-              py-4
-              border border-grey-400
-              cursor-pointer
-              hover:border-grey-500
-              font-medium
-              text-grey-400
-              hover:text-grey-500
-            "
-          >
-            on
-          </div>
-          <div
-            v-if="!toggle"
-            @click="
-              () => {
-                toggle = !toggle;
-              }
-            "
-            class="
-              px-8
-              py-4
-              border border-grey-200
-              cursor-pointer
-              hover:border-grey-300
-              font-medium
-              text-grey-200
-              hover:text-grey-300
-            "
-          >
-            off
-          </div>
         </div>
       </div>
     </div>
@@ -74,15 +31,9 @@ export default defineComponent({
   name: 'InputRow',
   props: {
     deleteable: { type: Boolean, required: false, default: false },
-    toggleable: { type: Boolean, required: false, default: false },
     intended: { type: Boolean, required: false, default: false },
     text: { type: String, required: false, default: '' },
   },
   components: { XIcon },
-  data() {
-    return {
-      toggle: false,
-    };
-  },
 });
 </script>
