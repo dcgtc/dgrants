b878fbe4526ff9666fbde54baf6c74ce1ed4cdc8
Richard Schumann <schumanncombo@gmail.com>
2021-09-15 23:47:47 +0200

    ui for networkselector

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,6 +2,7 @@
   <About :showAbout="showAbout" @toggle-about="toggleAbout" />
   <div>
     <layout-header id="header" @toggle-about="toggleAbout" />
+    <NetworkSelector />
     <main id="app-main"><router-view /></main>
   </div>
 </template>
@@ -9,6 +10,7 @@
 <script lang="ts">
 import { defineComponent, onMounted } from 'vue';
 import About from './components/About.vue';
+import NetworkSelector from './components/NetworkSelector.vue';
 import LayoutHeader from './components/LayoutHeader.vue';
 import useCartStore from 'src/store/cart';
 import useSettingsStore from 'src/store/settings';
@@ -16,7 +18,7 @@ import useWalletStore from 'src/store/wallet';
 
 export default defineComponent({
   name: 'App',
-  components: { About, LayoutHeader },
+  components: { NetworkSelector, About, LayoutHeader },
   data() {
     return {
       showAbout: false,
---
+++ app/src/components/NetworkSelector.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="px-4 md:px-12 py-8 bg-grey-200 text-white">
+    <div class="block md:flex gap-x-8 items-center">
+      <div>
+        <p class="text-white bg-pink inline">Wrong Network!<br />Please switch Network to Rinkeby.</p>
+      </div>
+
+      <div class="ml-auto mt-4 md:mt-0">
+        <button class="btn text-left">
+          <NetworkIcon />
+          Rinkeby
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { NetworkIcon } from '@fusion-icons/vue/interface';
+
+export default defineComponent({
+  name: 'NetworkSelector',
+  components: { NetworkIcon },
+  props: {},
+});
+</script>
