c82d6aa8bc9b5ffd0c79110a6262d7e9b4b1ce85
Graham Dixon <graham@gdixon.co.uk>
2021-09-16 07:21:07 +0100

    Implements ui for dgrants-rounds-list

--- app/src/views/GrantRoundsList.vue
+++ app/src/views/GrantRoundsList.vue
@@ -5,10 +5,10 @@
 
     <!-- Status filters -->
     <BaseFilterNav :active="selectedTab" :items="grantRoundsNav" />
-    <div>
+    <div class="max-w-screen-lg mx-auto">
       <!-- Status filtered Cards -->
       <template v-if="grantRoundLists[selectedTab] && grantRoundLists[selectedTab]?.rounds?.length">
-        <ul class="base-grid">
+        <ul class="text-left grid grid-cols-1 gap-10 py-12 px-4 md:px-12 md:grid-cols-2">
           <li v-for="(grantRound, index) in grantRoundLists[selectedTab].rounds" :key="grantRound.address">
             <GrantRoundCard
               :id="index"
@@ -24,7 +24,7 @@
       </template>
       <!-- Empty state -->
       <template v-else>
-        <div class="px-4 md:px-12 mt-8">
+        <div class="my-10 mx-4">
           <span>No {{ grantRoundLists[selectedTab].title }} Grant Rounds</span>
         </div>
       </template>
