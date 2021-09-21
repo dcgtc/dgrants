9db8e5e623e135bf659af4b74360ab44d15e36f7
Matt Solomon <matt@mattsolomon.dev>
2021-09-21 10:50:41 -0700

    fix: start polling even when user has never connected a wallet

--- app/src/App.vue
+++ app/src/App.vue
@@ -13,6 +13,7 @@ import About from './components/About.vue';
 import NetworkSelector from './components/NetworkSelector.vue';
 import LayoutHeader from './components/LayoutHeader.vue';
 import useCartStore from 'src/store/cart';
+import useDataStore from 'src/store/data';
 import useSettingsStore from 'src/store/settings';
 import useWalletStore from 'src/store/wallet';
 
@@ -25,11 +26,13 @@ export default defineComponent({
     };
   },
   setup() {
-    // Load cart, load settings, and try connecting user's wallet on page load
+    // Start polling, load cart, load settings, and try connecting user's wallet on page load
     const { initializeCart } = useCartStore();
+    const { startPolling } = useDataStore();
     const { connectWallet } = useWalletStore();
     const { lastWallet, initializeSettings } = useSettingsStore();
     onMounted(async () => {
+      startPolling();
       initializeCart();
       await initializeSettings();
       if (lastWallet.value) await connectWallet(lastWallet.value);
