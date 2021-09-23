7fbbcfc4515de3739a84b5c41b60d794355c34ec
Graham Dixon <graham@gdixon.co.uk>
2021-09-23 01:01:01 +0100

    Stores grants as {grantId: grant} in ls

--- app/src/utils/data/grants.ts
+++ app/src/utils/data/grants.ts
@@ -1,6 +1,6 @@
 // --- Types ---
 import { Grant } from '@dgrants/types';
-import { LocalStorageData } from 'src/types';
+import { LocalStorageData, LocalStorageAnyObj } from 'src/types';
 // --- Utils ---
 import { syncStorage } from 'src/utils/data/utils';
 import { BigNumber, Event } from 'ethers';
@@ -14,20 +14,6 @@ import useWalletStore from 'src/store/wallet';
 const { grantRegistry } = useWalletStore();
 
 /**
- * @notice Given a grants tx event - get only the named args
- *
- * @param {Event} tx The current state
- */
-const mapArgs = (tx: Event) => {
-  return {
-    id: tx.args?.id,
-    owner: tx.args?.owner,
-    payee: tx.args?.payee,
-    metaPtr: tx.args?.metaPtr,
-  } as Grant;
-};
-
-/**
  * @notice Get/Refresh all Grants
  *
  * @param {number} blockNumber The current blockNumber
@@ -38,20 +24,17 @@ export async function getAllGrants(blockNumber: number, forceRefresh = false) {
     allGrantsKey,
     {
       blockNumber: blockNumber,
-      ts: Date.now() / 1000,
     },
-    async (localStorageData?: LocalStorageData | undefined, save?: () => void) => {
+    async (localStorageData?: LocalStorageData | undefined, save?: (saveData: LocalStorageAnyObj) => void) => {
       // use the ls_blockNumber to decide if we need to update the grants
-      const ls_blockNumber = localStorageData?.blockNumber || START_BLOCK;
-      // only update grants if new ones are added...
-      let grants = localStorageData?.data?.grants || [];
-      // each update should be pulled in when we hydrate localStorage state
-      let grantUpdates: Grant[] = [];
+      const ls_blockNumber = localStorageData?.blockNumber || 0;
+      // pull the indexed grants data from localStorage
+      const ls_grants = localStorageData?.data?.grants || {};
       // every block
       if (forceRefresh || !localStorageData || (localStorageData && ls_blockNumber < blockNumber)) {
         // get the most recent block we collected
         const fromBlock = ls_blockNumber + 1 || START_BLOCK;
-        // get new state
+        // pull any newly created or edited grants from all blocks since we last polled
         const [newGrants, updatedGrants] = await Promise.all([
           grantRegistry.value?.queryFilter(
             grantRegistry?.value?.filters?.GrantCreated(null, null, null, null),
@@ -64,35 +47,41 @@ export async function getAllGrants(blockNumber: number, forceRefresh = false) {
             blockNumber
           ),
         ]);
-        // get any new donations to the grantRound
-        grants = [...grants, ...(newGrants || []).map(mapArgs)];
-        // check up updated grants
-        grantUpdates = (updatedGrants || []).map(mapArgs);
+        // set the mapped entries into the indexed grants data
+        [...newGrants, ...updatedGrants]
+          .map((tx: Event) => {
+            return {
+              id: tx.args?.id,
+              owner: tx.args?.owner,
+              payee: tx.args?.payee,
+              metaPtr: tx.args?.metaPtr,
+            } as Grant;
+          })
+          .forEach((grant) => {
+            ls_grants[BigNumber.from(grant.id).toString()] = grant as Grant;
+          });
       }
 
       // hydrate data from localStorage
-      const ls_grants = {
-        grants: grants.map((grant: Grant) => {
-          const updatedGrant = grantUpdates.find((foundGrant) => {
-            return BigNumber.from(grant.id).toString() === BigNumber.from(foundGrant.id).toString();
-          });
-
+      const grants = {
+        grants: (Object.values(ls_grants) as Grant[]).map((grant) => {
           return {
+            ...grant,
+            // return the id as a BigNumber
             id: BigNumber.from(grant.id),
-            owner: updatedGrant ? updatedGrant.owner : grant.owner,
-            payee: updatedGrant ? updatedGrant.payee : grant.payee,
-            metaPtr: updatedGrant ? updatedGrant.metaPtr : grant.metaPtr,
           } as Grant;
         }),
       };
 
       // conditionally save the state
-      if (grants.length && save) {
-        save();
+      if (grants.grants.length && save) {
+        save({
+          grants: ls_grants,
+        });
       }
 
       // returns all Grants[]
-      return ls_grants;
+      return grants;
     }
   );
 }
