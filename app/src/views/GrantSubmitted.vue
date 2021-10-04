5991ad6d1b73a580569962c53204d8ac95e78771
mkdir <projectdevelopinginbox@gmail.com>
2021-10-04 09:44:32 -0500

    grant submitted page

---
+++ app/src/views/GrantSubmitted.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <BaseHeader
+      name="Thank you for your grant submission! Your grant has been queue'd for review and upon approval will be visible to everyone. Average time to review is less than 24 hours."
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BaseHeader from 'src/components/BaseHeader.vue';
+export default defineComponent({
+  name: 'GrantRegistryNewGrant',
+  components: {
+    BaseHeader,
+  },
+});
+</script>
