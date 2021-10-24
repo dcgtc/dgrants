c44f841a0d32fdc7d459f5b63a5d80be79b24a0a
Richard Schumann <schumanncombo@gmail.com>
2021-10-24 22:50:49 +0200

    Gitcoin DAO Footer

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,6 +4,7 @@
     <layout-header id="header" @toggle-about="toggleAbout" />
     <NetworkSelector />
     <main id="app-main"><router-view /></main>
+    <layout-footer />
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import { defineComponent, ref, onMounted } from 'vue';
 import About from './components/About.vue';
 import NetworkSelector from './components/NetworkSelector.vue';
 import LayoutHeader from './components/LayoutHeader.vue';
+import LayoutFooter from './components/LayoutFooter.vue';
 import useCartStore from 'src/store/cart';
 import useDataStore from 'src/store/data';
 import useSettingsStore from 'src/store/settings';
@@ -19,7 +21,7 @@ import useWalletStore from 'src/store/wallet';
 
 export default defineComponent({
   name: 'App',
-  components: { About, LayoutHeader, NetworkSelector },
+  components: { About, LayoutHeader, LayoutFooter, NetworkSelector },
   setup() {
     const showAbout = ref(false);
     // Start polling, load cart, load settings, and try connecting user's wallet on page load
---
+++ app/src/components/LayoutFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="mt-12 py-12 border-t border-grey-100">
+    <div class="grid items-center justify-center">
+      <a href="https://gitcoindao.com" target="_blank" class="group">
+        <div class="flex flex-wrap gap-4 items-center justify-center">
+          <img class="w-5 xl:w-6" src="/gitcoindao.svg" alt="gitcoindao" />
+          <span class="text-center text-grey-400 group-hover:text-grey-500">Built by the Gitcoin community.</span>
+        </div>
+      </a>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+// --- External Imports ---
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LayoutFooter',
+});
+</script>
