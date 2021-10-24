5e632e1bea5f7ff9b913c75fcdd7cc2dfb1cbdd5
Richard Schumann <schumanncombo@gmail.com>
2021-10-24 17:01:47 +0200

    contribution success / faq page enhancements
    - closes #431
    
    - fixed the issue for weirded scaling of that page hopefully ( can not really test on physical mobile from local env ... ) by having a flex-wrap for the button-row wrapping div .
    
    - changed to some nicer text for the thank you page, thank-you description, added icons to the buttons for consistency
    
    - changed also the FAQ page in this commit - to have same semantical structure for both static centered pages. added some nicer text, and some icons for the buttons for consistency too.

--- app/src/views/Error404.vue
+++ app/src/views/Error404.vue
@@ -1,12 +1,25 @@
 <template>
   <section class="px-4 md:px-12 py-4 md:py-12">
     <div class="grid justify-center text-center">
-      <h1 class="text-grey-500">404</h1>
-      <p>Page not found!</p>
+      <h1>This page ist lost.</h1>
 
-      <div class="mt-8 flex gap-4 justify-center">
-        <button @click="routerGo(-1)" class="btn">Back</button>
-        <button @click="routerPush('/')" class="btn">Home</button>
+      <p class="mt-4 italic text-grey-400 mx-auto max-w-6xl">
+        We’ve explored deep and wide, but we can’t find the page you were looking for.
+      </p>
+
+      <!-- button row container -->
+      <div class="mt-12 flex flex-wrap gap-8 justify-center">
+        <!--back button-->
+        <button @click="routerGo(-1)" class="btn">
+          <BackIcon class="icon-small" />
+          <span>Back</span>
+        </button>
+
+        <!--home button-->
+        <button @click="routerPush('/')" class="btn">
+          <HomeIcon class="icon-small" />
+          <span>Home</span>
+        </button>
       </div>
     </div>
   </section>
@@ -15,6 +28,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useRouter } from 'vue-router';
+import { HomeIcon, ArrowLeftIcon as BackIcon } from '@fusion-icons/vue/interface';
 
 export default defineComponent({
   name: 'Error404',
@@ -24,5 +38,6 @@ export default defineComponent({
     return { routerPush: router.push, routerGo: router.go };
     /* eslint-enable @typescript-eslint/unbound-method */
   },
+  components: { HomeIcon, BackIcon },
 });
 </script>
