42d7da577930e8b281fad9443631684b4feefe85
Richard Schumann <schumanncombo@gmail.com>
2021-11-05 17:09:06 +0100

    updating footer with gitcoin dao badge
    + removing vue favicon ^^

--- app/src/components/LayoutFooter.vue
+++ app/src/components/LayoutFooter.vue
@@ -1,11 +1,8 @@
 <template>
   <footer class="mt-12 py-12 border-t border-grey-100">
     <div class="grid items-center justify-center">
-      <a href="https://gitcoindao.com" target="_blank" class="group">
-        <div class="flex flex-wrap gap-4 items-center justify-center">
-          <img class="w-5 xl:w-6" src="/gitcoindao.svg" alt="gitcoindao" />
-          <span class="text-center text-grey-400 group-hover:text-grey-500">Built by the Gitcoin community.</span>
-        </div>
+      <a href="https://gitcoindao.com" target="_blank" rel="noreferrer noopener">
+        <img src="/gitcoindao.svg" alt="gitcoin dao" />
       </a>
     </div>
   </footer>
