f1e37f1ba2d8f1ba7cd72a2630b12a45f8117f01
Aditya Anand M C <aditya.anandmc@gmail.com>
2021-11-12 19:44:47 +0530

    update fleek URL

--- utils/src/ipfs.ts
+++ utils/src/ipfs.ts
@@ -1,12 +1,12 @@
 import { create } from 'ipfs-http-client';
 
-const ipfsEndpoint = 'https://ipfs-api.dev.fleek.cool';
+const ipfsEndpoint = 'https://ipfs-api.fleek.cool';
 
 export const createIpfs = (fleekApiKey: string) => {
   return create({
     url: ipfsEndpoint,
     headers: {
-      Authorization: `v2 ${fleekApiKey}`,
+      Authorization: fleekApiKey,
     },
   });
 };
