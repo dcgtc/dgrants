77b25a5617da27876e60522c4ffa486967509ef8
Matt Solomon <matt@mattsolomon.dev>
2021-11-17 09:38:06 -0800

    fix: twitter links now point to gitcoindao, not gitcoin

--- app/src/components/About.vue
+++ app/src/components/About.vue
@@ -47,7 +47,7 @@
             </button></a
           >
 
-          <a href="https://twitter.com/gitcoin" target="_blank" rel="noreferrer noopener">
+          <a href="https://twitter.com/gitcoindao" target="_blank" rel="noreferrer noopener">
             <button class="btn">
               <TwitterIcon />
               <span>Twitter</span>
