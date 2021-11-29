28b6137e3dbb8b852c9155e8ca7dde143705174a
git <jjwozniak91@gmail.com>
2021-11-29 10:21:24 -0500

    feature/my-contributions: deleted unnecessary comments to address PR suggestion

--- app/src/views/Contribution.vue
+++ app/src/views/Contribution.vue
@@ -1,21 +1,9 @@
 <template>
   <template v-if="fullContributions && !loading">
-    <!-- this is a header for a the route "/contributions" what show ALL contributions.
-      other pages already have a header, so its not needed there. -->
     <BaseHeader :name="title" />
-
-    <!-- btw in this component was a "cursor-pointer" for some reason. i removed it as this is
-    not something clickable. just for final PR this component could need that update too-->
     <SectionHeader :title="`Contributions (${contributionTotal})`" />
-
-    <!-- a single contribution ( right now this would be the "ContributionRow.vue" what we maybe
-    not even wana keep - depends on the architecture you wana do ...
-    i added a v-for loop with some dummy data for {{name}} just to test how this looks
-    you can delete this of cause ...
-    -->
     <ContributionDetail :contributions="fullContributions" />
   </template>
-
   <LoadingSpinner v-else />
 </template>
 
